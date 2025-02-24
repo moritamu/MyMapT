ce70270b4b170c21f65c16cb2b5113cae64a3cce
morisum <moris@r.sannet.ne.jp>
2025-02-24 09:28:21 +0900

    完成版 このあとは修正版

--- MyMapT/ContentView.swift
+++ MyMapT/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State var inputText: String = ""
     @State var displaySearchKey: String = "桶川市役所"
+    @State var displayMapType: MapType = .standard
+
+
     var body: some View {
         VStack {
             TextField("キーワード", text: $inputText, prompt: Text("検索したい場所を typing..."))
@@ -17,7 +20,26 @@ struct ContentView: View {
                     displaySearchKey = inputText
                 }
                 .padding()
-            MapView(searchKey: displaySearchKey)
+
+            ZStack(alignment: .bottomTrailing) {
+                MapView(searchKey: displaySearchKey, mapType: displayMapType)
+
+                Button {
+                    if displayMapType == .standard {
+                        displayMapType = .satellite
+                    } else if displayMapType == .satellite {
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                } label: {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0)
+                }
+                .padding(.trailing, 20.0)
+                .padding(.bottom, 30.0)
+            }
         }
     }
 }
--- MyMapT/MapView.swift
+++ MyMapT/MapView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 import MapKit
 
+enum MapType {
+    case standard
+    case satellite
+    case hybrid
+}
+
 struct MapView: View {
     let searchKey: String
+    let mapType: MapType
+
     @State var targetCoordinate = CLLocationCoordinate2D()
     @State var cameraPosition: MapCameraPosition = .automatic
+
+    var mapStyle: MapStyle {
+        switch mapType {
+        case .standard:
+            return MapStyle.standard()
+        case .satellite:
+            return MapStyle.imagery()
+        case .hybrid:
+            return MapStyle.hybrid()
+        }
+    }
 
     var body: some View {
         Map(position: $cameraPosition) {
             Marker(searchKey, coordinate: targetCoordinate)
         }
+        .mapStyle(mapStyle)
         .onChange(of: searchKey, initial: true) { oldValue,newValue in
             print("検索キーワード：\(newValue)")
 
@@ -41,5 +61,5 @@ struct MapView: View {
 }
 
 #Preview {
-    MapView(searchKey: "桶川市役所")
+    MapView(searchKey: "桶川市役所", mapType: .standard)
 }
